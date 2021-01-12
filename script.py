b468729b9fa2cacb5bcd5c5c79bdb7e7faf1c007
Jawad Rizvi <jawadrizvi40@gmail.com>
2021-01-11 19:38:38 -0700

    Up to 7

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 import requests
 
 print(requests.__version__)
+homePageUrl = "http://www.google.com/"
+
+response = requests.get(homePageUrl)
+
+# reference: http://zetcode.com/python/requests/
+print(response.text)
