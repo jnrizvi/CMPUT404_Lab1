caddaa482aefa560ae7852f4cd4c3f1eb4c55da9
Jawad Rizvi <jawadrizvi40@gmail.com>
2021-01-11 19:56:24 -0700

    I think I'm finished

--- script.py
+++ script.py
@@ -7,3 +7,8 @@ response = requests.get(homePageUrl)
 
 # reference: http://zetcode.com/python/requests/
 print(response.text)
+
+rawGHURL = "https://raw.githubusercontent.com/jnrizvi/CMPUT404_Lab1/main/script.py"
+response = requests.get(rawGHURL)
+
+print(response.text)
